c6091b60767724f44a4c0d1f7ce8677efb2180b9
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2018-03-10 22:45:48 -0500

    Add better documentation and clean code layout

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
-
 const cardTypes = [
     'diamond',
     'anchor',
@@ -23,6 +19,22 @@ for(let i = 0; i < cardTypes.length; ++i){
   cards.push(cardTypes[i]);
 }
 
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+// Generates a new deck and renders it to the deck's DOM
 function generateDeck() {
     const deck = document.getElementsByClassName('deck')[0];
 
@@ -52,31 +64,7 @@ function generateDeck() {
     }
 }
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-restartButton = document.getElementsByClassName('restart')[0];
-restartButton.addEventListener('click', initializeGame);
-
-const timerIncrement = window.setInterval(increaseTimer, 1000);
+// Resets the game state
 function initializeGame() {
     lockedCards = [];
     activeCard = null;
@@ -88,55 +76,21 @@ function initializeGame() {
     restartTimer();
 }
 
-function increaseTimer() {
-    timerCounter = document.getElementsByClassName('timer')[0];
-    timerCounter.innerHTML = String(getTime() + 1);
-}
-
-function getTime() {
-    timerCounter = document.getElementsByClassName('timer')[0];
-    return Number(timerCounter.innerHTML);
-}
-
-function restartTimer() {
-    timerCounter = document.getElementsByClassName('timer')[0];
-    timerCounter.innerHTML = String(0);
-}
-
-function getMoves() {
-    movesCounter = document.getElementsByClassName('moves')[0];
-    return Number(movesCounter.innerHTML);
-}
-
-function setMovesTo(number) {
-    movesCounter = document.getElementsByClassName('moves')[0];
-    movesCounter.innerHTML = String(number);
-}
-
-function getStars() {
-    const stars = document.getElementsByClassName('stars')[0];
-    return stars.getElementsByClassName('fa-star').length;
-}
-
-function setStarsTo(number) {
-    const stars = document.getElementsByClassName('stars')[0];
-    for(let i = 0; i < 3; i++){
-        if (i < number) {
-            stars.children[i].firstElementChild.className = 'fa fa-star';
-        } else {
-            stars.children[i].firstElementChild.className = 'fa fa-star-o';
-        }
-    }
-}
+// Identify restart button and attach the initializeGame function to it
+restartButton = document.getElementsByClassName('restart')[0];
+restartButton.addEventListener('click', initializeGame);
 
 function cardClickHandler(event) {
     let cardElement = event.target;
 
+    // Sometimes the target was the inner 'i' element (icon), so lets jump up
+    // to the parent node (the card)
     if (!cardElement.classList.contains('card')) {
         cardElement = cardElement.parentNode;
     }
 
-    if (cardElement === activeCard) { return }
+    // Prevent issues when clicking the active card again
+    if (cardElement === activeCard) { return; }
 
     showCard(cardElement);
 
@@ -150,7 +104,7 @@ function cardClickHandler(event) {
         setMovesTo(getMoves() + 1);
         evaluateStars();
 
-        if (isGameWon()) { showWonModal() };
+        if (isGameWon()) { showWonModal(); };
 
         activeCard = null;
     } else {
@@ -158,6 +112,106 @@ function cardClickHandler(event) {
     }
 }
 
+
+/* -- Timer Functionality Start -- */
+// Increase the timer's rendered text by one
+function increaseTimer() {
+    timerCounter = document.getElementsByClassName('timer')[0];
+    timerCounter.innerHTML = String(getTime() + 1);
+}
+
+// Creates an interval timer for 1 seconds to increase the timer's DOM value
+const timerIncrement = window.setInterval(increaseTimer, 1000);
+
+// Get the timer's value
+function getTime() {
+    timerCounter = document.getElementsByClassName('timer')[0];
+    return Number(timerCounter.innerHTML);
+}
+
+// Restart the timer -- back to 0
+function restartTimer() {
+    timerCounter = document.getElementsByClassName('timer')[0];
+    timerCounter.innerHTML = String(0);
+}
+/* -- Timer Functionality End -- */
+
+
+/* -- Moves Functionality Start -- */
+// Get the number of moves made from the rendered DOM
+function getMoves() {
+    movesCounter = document.getElementsByClassName('moves')[0];
+    return Number(movesCounter.innerHTML);
+}
+
+// Sets the number of moves to the rendered DOM
+function setMovesTo(number) {
+    movesCounter = document.getElementsByClassName('moves')[0];
+    movesCounter.innerHTML = String(number);
+}
+/* -- Moves Functionality End -- */
+
+
+/* -- Stars Functionality Start -- */
+// Get the number of stars that are present on the DOM -- for points
+function getStars() {
+    const stars = document.getElementsByClassName('stars')[0];
+    return stars.getElementsByClassName('fa-star').length;
+}
+
+// Sets the number of stars to the specified value, this is out of 3 and thus
+// the rest of the stars are left as outlines
+function setStarsTo(number) {
+    const stars = document.getElementsByClassName('stars')[0];
+    for(let i = 0; i < 3; i++){
+        if (i < number) {
+            stars.children[i].firstElementChild.className = 'fa fa-star';
+        } else {
+            stars.children[i].firstElementChild.className = 'fa fa-star-o';
+        }
+    }
+}
+/* -- Stars Functionality End -- */
+
+
+/* -- Cards Functionality Start -- */
+// If the selected and active cards match, we need to lock them and make them
+// known as matches
+function cardsMatching(cardElement) {
+    lockedCards.push(cardElement);
+    lockedCards.push(activeCard);
+    cardElement.classList.add('match');
+    activeCard.classList.add('match');
+}
+
+// If the cards are not matching, we need to hide both cards -- a timeout is
+// used so that the animation can play out and shows the selected card
+function cardsNotMatching(cardElement) {
+    window.setTimeout(hideCard.bind(this, cardElement), 500);
+    window.setTimeout(hideCard.bind(this, activeCard), 500);
+}
+
+// A check to see if the selected card matches the active card
+function isMatchingCard(cardElement) {
+    return activeCard.firstChild.className === cardElement.firstChild.className;
+}
+
+// Shows a card from a CSS perspective
+function showCard(cardElement) {
+    cardElement.classList.add('open');
+    cardElement.classList.add('show');
+}
+
+// Hides a card from a CSS perspective
+function hideCard(cardElement) {
+    cardElement.classList.remove('open');
+    cardElement.classList.remove('show');
+}
+/* -- Cards Functionality End -- */
+
+
+/* -- Points and Win Condition Functionality Start -- */
+// Check the number of moves made, and adjust the points
 function evaluateStars() {
     const currentMoves = getMoves();
 
@@ -170,56 +224,38 @@ function evaluateStars() {
     }
 }
 
-function cardsMatching(cardElement) {
-    lockedCards.push(cardElement);
-    lockedCards.push(activeCard);
-    cardElement.classList.add('match');
-    activeCard.classList.add('match');
-}
-
-function cardsNotMatching(cardElement) {
-    window.setTimeout(hideCard.bind(this, cardElement), 500);
-    window.setTimeout(hideCard.bind(this, activeCard), 500);
-}
-
-function showCard(cardElement) {
-    cardElement.classList.add('open');
-    cardElement.classList.add('show');
-}
-
-function hideCard(cardElement) {
-    cardElement.classList.remove('open');
-    cardElement.classList.remove('show');
-}
-
-function isMatchingCard(cardElement) {
-    return activeCard.firstChild.className === cardElement.firstChild.className;
-}
-
+// Check if the game has been won -- look if the number of locked cards matches
+// the number of total cards in play
 function isGameWon() {
     return lockedCards.length === cards.length;
 }
+/* -- Points and Win Condition Functionality End -- */
 
+
+/* -- Modal Functionality Start -- */
+// Show the won modal and populate the dynamic values
 function showWonModal() {
-    let modal = document.getElementById('modal');
+    const modal = document.getElementById('modal');
     modal.style.display = 'block';
 
-    let movesScore = document.getElementById('moves-score');
-    let starsScore = document.getElementById('stars-score');
-    let timeScore = document.getElementById('time-score');
+    const movesScore = document.getElementById('moves-score');
+    const starsScore = document.getElementById('stars-score');
+    const timeScore = document.getElementById('time-score');
 
     movesScore.innerText = getMoves();
     starsScore.innerText = getStars();
     timeScore.innerText = getTime();
 }
 
+// Connect the play again button with a click handler than re-initializes the
+// game and closes the modal
 const playAgainButton = document.getElementById('play-again-button');
 playAgainButton.addEventListener('click', function(event) {
-    let modal = document.getElementById('modal');
+    const modal = document.getElementById('modal');
     modal.style.display = 'none';
     initializeGame();
 });
+/* -- Modal Functionality End -- */
 
-// TODO: README + Comment Documentation
-
+// Ready and start the game
 initializeGame();
